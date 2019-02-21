25ef9ee8fcbbed4fcfdea67440657085d6a113e7
Alexandre CONTASSOT* <alexandrecontassot@hotmail.fr>
2019-02-21 12:03:12 +0100

    rectangle panel

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 using namespace cv;
 using namespace std;
@@ -14,6 +15,8 @@ VideoCapture capture;
 void onTrackbarSlide(int count, void* data) {
     capture.set(CAP_PROP_POS_FRAMES, count);
 }
+
+double getCosAlKashi (const Point &A, const Point &B, const Point &C);
 
 int main() {
     std::string winName = "video.MOV";
@@ -41,7 +44,7 @@ int main() {
         Canny( frameGray, canny_out, 80, 160, 3);
 
         // Faire une dilatation
-        Mat dilatation_element = getStructuringElement( MORPH_ELLIPSE, Size( 2*3 + 1, 2*3+1 ), Point( 3, 3));
+        Mat dilatation_element = getStructuringElement( MORPH_ELLIPSE, Size( 7, 7 ), Point( 3, 3));
 
         dilate( canny_out, dilation_out, dilatation_element );
 
@@ -60,10 +63,18 @@ int main() {
             approxPolyDP(contours[i], courbe, 0.02*perimetre, true);
             if (contourArea(courbe) > 5000 && isContourConvex(courbe)) {
                 Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-                drawContours(shape_out, contours, (int)i, color, 4, 8, hierarchy, 0, Point());
+                drawContours(shape_out, contours, static_cast<int>(i), color, 4, 8, hierarchy, 0, Point());
+                if (courbe.size() == 3) // c'est un triangle
+                    cv::putText(shape_out, "TRIANGLE", courbe[0], cv::FONT_HERSHEY_PLAIN, 2, cv::Scalar(0, 255, 0));
+                if (courbe.size() == 4) { // c'est peut être un rectangle
+                    double cos1 = getCosAlKashi(courbe[2], courbe[0], courbe[1]);
+                    double cos2 = getCosAlKashi(courbe[3], courbe[1], courbe[2]);
+                    // or si un quadrilatère a deux angle droit alors , tous ces angles sont droits
+                    if (cos1 < 0.1 && cos1 > -0.1 && cos2 < 0.1 && cos2 > -0.1) // c'est un rectangle
+                        cv::putText(shape_out, "RECTANGLE", courbe[0], cv::FONT_HERSHEY_PLAIN, 2, cv::Scalar(0, 255, 0));
+                }
             }
-            if (courbe.size() == 3)
-                cv::putText(shape_out, "TRIANGLE", courbe[0], cv::FONT_HERSHEY_PLAIN, 2, cv::Scalar(0, 255, 0));
+
         }
 
         // Affichages
@@ -85,4 +96,12 @@ int main() {
 
     return 0;
 
+}
+
+double getCosAlKashi (const Point &A, const Point &B, const Point &C) {
+    double CA = cv::norm(Mat(A), Mat(C));
+    double CB = cv::norm(Mat(B), Mat(C));
+    double AB = cv::norm(Mat(B), Mat(A));
+
+    return (pow(CA, 2) + pow(CB, 2) - pow(AB, 2))/(2*CA*CB);
 }
